ae9e15ed3d0f271a1c650546a8065058cece2b72
Michał Żukowski <miczukow@wp.pl>
2018-08-20 14:29:06 +0200

    Add password generating method
    * Added password generating method with random symbols from ASCII table 33 - 126

--- src/main/java/PassGens.java
+++ src/main/java/PassGens.java
@@ -3,13 +3,13 @@ import java.util.Random;
 public class PassGens {
 
     //Method generates password with given length containing just letters [a-zA-Z].
-    public static String passGenJustLetters(int lenght){
+    public static String passGenJustLetters(int length){
         String pass="";
         Random generator = new Random();
         char tmpCh;
         int tmpInt;
 
-        for(int i = 0; i < lenght; ++i){
+        for(int i = 0; i < length; ++i){
             tmpInt = generator.nextInt(52);
             if(tmpInt < 26)
                 tmpCh = (char) (tmpInt + 65);
@@ -17,18 +17,17 @@ public class PassGens {
                 tmpCh = (char) (tmpInt - 26 + 97);
             pass += tmpCh;
         }
-
         return pass;
     }
 
     //Method generates password with given length containing letters and digits [a-zA-Z0-9].
-    public static String passGenLettersAndDigits(int lenght){
+    public static String passGenLettersAndDigits(int length){
         String pass = "";
         Random generator = new Random();
         char tmpCh;
         int tmpInt;
 
-        for(int i = 0; i < lenght; ++i){
+        for(int i = 0; i < length; ++i){
             tmpInt = generator.nextInt(62);
             if(tmpInt < 26)
                 tmpCh = (char) (tmpInt + 65);
@@ -37,6 +36,19 @@ public class PassGens {
             else
                 tmpCh = (char)(tmpInt - 52 + 48);
             pass += tmpCh;
+        }
+        return pass;
+    }
+
+    //Method generates password with given length containig letters, digits and other symbols ASCII 33-126
+    public static String passGenAllSymbols(int length){
+        String pass = "";
+        Random generator = new Random();
+        int tmpInt;
+
+        for(int i = 0; i < length; ++i){
+            tmpInt = generator.nextInt(94);
+            pass += (char)(tmpInt + 33);
         }
         return pass;
     }
