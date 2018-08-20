571ca889395b126eea34742c4c63ca9d2b901c2b
Michał Żukowski <miczukow@wp.pl>
2018-08-20 12:56:23 +0200

    Add password generating method
    *Added password generating method with letters and digits

--- src/main/java/PassGens.java
+++ src/main/java/PassGens.java
@@ -22,8 +22,22 @@ public class PassGens {
     }
 
     //Method generates password with given length containing letters and digits [a-zA-Z0-9].
-    public static String passGenLettersAndDigits(int length){
+    public static String passGenLettersAndDigits(int lenght){
         String pass = "";
+        Random generator = new Random();
+        char tmpCh;
+        int tmpInt;
+
+        for(int i = 0; i < lenght; ++i){
+            tmpInt = generator.nextInt(60);
+            if(tmpInt < 26)
+                tmpCh = (char) (tmpInt + 65);
+            else if(tmpInt > 25 && tmpInt < 51)
+                tmpCh = (char) (tmpInt - 25 + 97);
+            else
+                tmpCh = (char)(tmpInt - 50 + 48);
+            pass += tmpCh;
+        }
         return pass;
     }
 }
