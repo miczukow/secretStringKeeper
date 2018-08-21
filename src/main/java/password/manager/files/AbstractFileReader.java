7f4f3b0386cac211783721672a778cf2d0a560ce
miczukow <miczukow@wp.pl>
2018-08-21 11:51:22 +0200

    Feature/file readers (#4)
    * Add reading file methods
    
    * Added FileReader interface
    * Added AbstractFileReader class
    * Added BufferedFileReader class
    * Added LinesFileReader class
    * Added BufferedFileReaderTest
    * Added LinesReaderTest

---
+++ src/main/java/password/manager/files/AbstractFileReader.java
@@ -0,0 +1,33 @@
+package password.manager.files;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractFileReader implements FileReader {
+
+    protected File getFile(String path){
+        if (path == null){
+            throw new IllegalArgumentException("May not be null");
+        }
+        ClassLoader classLoader = getClass().getClassLoader();
+        return new File(classLoader.getResource(path).getFile());
+//        return new File(path);
+    }
+
+    @Override
+    public List<PasswordEntry> getPasswordEntries(String path) throws IOException{
+        List<PasswordEntry> result = new ArrayList();
+        List<String> lines = read(path);
+
+        for(String line : lines){
+            String[] splitResult = line.split(";");
+            PasswordEntry passwordEntry = new PasswordEntry(splitResult[0], splitResult[1], splitResult[2]);
+            result.add(passwordEntry);
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/password/manager/files/FileReader.java
@@ -0,0 +1,12 @@
+package password.manager.files;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface FileReader {
+
+    List<String> read (String path) throws IOException;
+    List<PasswordEntry> getPasswordEntries(String path) throws IOException;
+
+
+}
